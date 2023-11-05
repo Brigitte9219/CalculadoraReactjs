75eb0a97812c3388b34ec474b42bbdc3a4521118
brigitte <mbpadilla4@misena.edu.co>
2023-11-04 21:10:54 -0500

    Proyecto calculadora interactiva

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import { evaluate } from 'mathjs';
+import { useState } from 'react';
+import './App.css';
+import Boton from './componentes/Boton';
+import BotonClear from './componentes/BotonClear';
+import Pantalla from './componentes/Pantalla';
+import freeCodeCampLogo from './img/logo.jpg';
+
+function App() {
+
+  const [input, setInput] = useState('');
+
+  const agregarInput = valor => {
+    setInput(input + valor);
+  };
+
+  const calcularResultado = () => {
+    if(input){
+    setInput(evaluate(input));
+  }else{
+    alert('Por favor ingrese valores para realizar los cálculos');
+  }
+  };
+
+  return (
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+        src={freeCodeCampLogo}
+        className='freecodecamp-logo'
+        alt='Logo de freeCodeCamp' />
+      </div>
+      <div className='contenedor-calculadora'>
+        <Pantalla input={input} />
+        <div className='fila'>
+        <Boton manejarClic={agregarInput}>1</Boton>
+        <Boton manejarClic={agregarInput}>2</Boton>
+        <Boton manejarClic={agregarInput}>3</Boton>
+        <Boton manejarClic={agregarInput}>+</Boton>
+        </div>
+        <div className='fila'>
+        <Boton manejarClic={agregarInput}>4</Boton>
+        <Boton manejarClic={agregarInput}>5</Boton>
+        <Boton manejarClic={agregarInput}>6</Boton>
+        <Boton manejarClic={agregarInput}>-</Boton>
+        </div>
+        <div className='fila'>
+        <Boton manejarClic={agregarInput}>7</Boton>
+        <Boton manejarClic={agregarInput}>8</Boton>
+        <Boton manejarClic={agregarInput}>9</Boton>
+        <Boton manejarClic={agregarInput}>*</Boton>
+        </div>
+        <div className='fila'>
+        <Boton manejarClic={calcularResultado}>=</Boton>
+        <Boton manejarClic={agregarInput}>0</Boton>
+        <Boton manejarClic={agregarInput}>.</Boton>
+        <Boton manejarClic={agregarInput}>/</Boton>
+        </div>
+        <div className='fila'>
+          <BotonClear manejarClear={()=> setInput('')}>
+            Clear
+            </BotonClear>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
